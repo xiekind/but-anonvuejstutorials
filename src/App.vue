4b4ecd177bd3a001d0efe743aee12eff42a86120
XhaviaKind <07judilynb@gmail.com>
2019-09-17 12:25:56 +0800

    add all

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/kind.png">
+    <HelloWorld msg="hi Judy Welcome to your first Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/Login.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+// import Login from './components/Login.vue'
 
 export default {
   name: 'app',
---
+++ src/components/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="login">
+    <h1>{{ "Login Page" }}</h1>
+    
+    <button  class="btn btn-success">Login</button>
+    <form>
+
+    </form>
+
+  </div>
+</template>
+
+<script>
+export default {
+    name : 'Login'
+}
+</script>
+
