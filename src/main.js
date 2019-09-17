a029fa5b1cf22c03f951c956f1eb4af4ee9d2930
XhaviaKind <07judilynb@gmail.com>
2019-09-17 13:25:47 +0800

    added sweetalert2. Implement ERROR,

--- src/main.js
+++ src/main.js
@@ -4,6 +4,13 @@ import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 Vue.use(BootstrapVue)
+import VueSweetalert2 from 'vue-sweetalert2';
+ 
+// If you don't need the styles, do not connect
+import 'sweetalert2/dist/sweetalert2.min.css';
+ 
+Vue.use(VueSweetalert2);
+
 
 
 Vue.config.productionTip = false
