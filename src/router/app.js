c90f987a84b3b60083a0aff09e974dad27582896
XhaviaKind <07judilynb@gmail.com>
2019-09-19 11:41:45 +0800

    added folder

---
+++ src/router/app.js
@@ -0,0 +1,21 @@
+import { resolve } from "path";
+
+// const User = {
+//     template: '<div>User</div>'
+//   }
+  
+//   const router = new VueRouter({
+//     routes: [
+//       // dynamic segments start with a colon
+//       { path: '/user/:id', component: User }
+//     ]
+//   })
+
+
+export default{
+    routes : [{
+        path: './login',
+        name: 'login',
+        component: resolve => require(['../modules/basic/'])
+    }]
+}
---
+++ src/router/module_routes.js
@@ -0,0 +1,21 @@
+import AUTH from '../services/auth'
+import { resolve } from 'dns'
+
+let beforeEnter = (to, from, next) => {
+    AUTH.currentPath = to.path
+}
+
+var devRoutes = []
+let app = require('./app.js')
+devRoutes = devRoutes.concat(app.default.routes)
+for(let x = 0; x < devRoutes.length; x++){
+    devRoutes[x]['beforeEnter'] = beforeEnter
+}
+let routes = [
+    {
+        path: '/',
+        name: 'home',
+        component: resolve => require('../modules/basic/Login.vue'), resolve,
+        beforeEnter
+    }
+]
